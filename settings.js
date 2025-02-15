70d2741e1cc1a5c290376d5d6d225a40ed8d18be
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-15 10:02:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Saurabh',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Saurabh Bot",
+  botOwner: 'Saurabh Thakur', // Your name
+  ownerNumber: '917887099893', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
