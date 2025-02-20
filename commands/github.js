a9e82672eefc2dd95bbbb5ca5ddbe87fddd4d7c5
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-21 00:38:52 +0530

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -16,7 +16,7 @@ _Star ⭐ the repository if you like the bot!_`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363322419190456@newsletter',
                     newsletterName: 'Saurabh-bot',
                     serverMessageId: -1
                 }
