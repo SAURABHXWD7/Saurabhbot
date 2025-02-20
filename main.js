3540bac94efd9931e4678e09cf98b016452eb05d
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-20 21:26:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ const { autoStatusCommand, handleStatusUpdate } = require('./commands/autostatus
 global.packname = settings.packname;
 global.author = settings.author;
 global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.ytch = "Mr Saurabh Thakur";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -84,7 +84,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'Saurabh-bot',
             serverMessageId: -1
         }
     }
